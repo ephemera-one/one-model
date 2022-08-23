38be45598712df9cd55939361bf14477bab38905
Vadim Smakhtin <vadim.smakhtin@habidatum.com>
2022-08-23 08:25:53 +0400

    Latest changes

--- render_script.py
+++ render_script.py
@@ -1,10 +1,14 @@
 import bpy
 
-bpy.ops.wm.open_mainfile(filepath="blender_projects/test/prototype.blend")
+bpy.context.scene.render.engine = "CYCLES"
+
+bpy.ops.wm.open_mainfile(filepath="blender_projects/test/nature_cognita_v45.blend")
 bpy.ops.preferences.addon_enable(module="sverchok-master")
 
 with open("blender_projects/test/data2.csv", "r") as f:
+    print(bpy.data.texts["data.csv"].as_string())
     data = f.read()
+    print(data)
     bpy.data.texts["data.csv"].from_string(data)
 
 s_nodes = bpy.data.node_groups[0]
